c55981c88f3ce16854bc4d49e24ecff26ed7deab
mrpyrex <ndifrekeumoren@neupytech.com>
2018-06-25 06:58:07 +0100

    Added pg database

--- neupytech/settings.py
+++ neupytech/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'neupytech.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neudb',
+        'USER': 'postgres',
+        'PASSWORD': 'Elyon2601',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -139,10 +150,6 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
-
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-#STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
 
 MEDIA_URL = "/media/"
 
