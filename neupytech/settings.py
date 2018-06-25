046b9042242ee5d2edfd36b5466d1a62a2aab277
mrpyrex <ndifrekeumoren@neupytech.com>
2018-06-25 20:45:20 +0100

    Fixed static

--- neupytech/settings.py
+++ neupytech/settings.py
@@ -141,19 +141,20 @@ USE_TZ = True
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'neupytech/static/'),
+]
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
 
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
+
 
 try:
     from .local_settings import *
