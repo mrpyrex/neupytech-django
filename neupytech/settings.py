fc375f4d1827e64a76d95920251389b87610020e
mrpyrex <ndifrekeumoren@neupytech.com>
2018-07-10 21:21:16 +0100

    Added posts app

--- neupytech/settings.py
+++ neupytech/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'pages',
     'contact',
     'team',
+    'posts',
     'ckeditor',
 ]
 
--- neupytech/urls.py
+++ neupytech/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('contact/', include('contact.urls')),
     path('team/', include('team.urls')),
+    path('posts/', include('posts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
