03c72e3e4ab7a706ef13019edb95744c284fcf8d
mrpyrex <ndifrekeumoren@neupytech.com>
2018-08-08 20:39:03 +0100

    Tweeks

--- pages/forms.py
+++ pages/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Join
 
 class JoinForm(forms.ModelForm):
+    email = forms.EmailField(label='', widget=forms.EmailInput(attrs={'class': 'materialize-input'}))
     class Meta:
         model = Join
         fields = ['email']
